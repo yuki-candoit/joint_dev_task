d9675efcef022589b5606891b114dea932fba19b
高山有貴 <yuki@YKMac.local>
2021-03-11 15:59:22 +0900

    add ruby q15,16 to task.rb

--- task.rb
+++ task.rb
@@ -132,7 +132,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.key?(:age)? "OK" : "NG"
+  puts data2.key?(:age)? "OK" : "NG"
 end
 
 def q16
@@ -144,7 +145,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do|user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+end
 end
 
 class UserQ17
